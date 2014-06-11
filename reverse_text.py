cb6d72148e10bf9b3ec4f151a345a32a741fdd1e
Joseph Jo <joseph.jo@coxinc.com>
2014-06-11 09:48:17 -0400

    Add function reverse_text

---
+++ reverse_text.py
@@ -0,0 +1,15 @@
+""" Reverses a given string """
+def reverse_text(text):
+    reversed_array = []
+    y = 1
+
+    for x in text:
+        while y < len(text) + 1:
+            reversed_array.append(text[-y])
+            y = y + 1
+
+    reverse = ''.join(reversed_array)
+    print reverse
+
+
+reverse_text("Hello world!")
