015e24726da109dc14354c4391b92f476f33d60d
Joseph Jo <joseph.jo@coxinc.com>
2014-06-12 22:06:25 -0400

    Add censor function

---
+++ censor.py
@@ -0,0 +1,8 @@
+""" Prints a string with a phrase censored """
+def censor(text, word):
+    if word in text:
+        censor = len(word) * '*'
+        print text.replace(word, censor)
+
+
+censor("Taco Bell Warns Employees Against Directly Exposing Skin To Food.", "Taco Bell")
