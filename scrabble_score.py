7bafc24c47bcdd85976bb462b9555274af9f6141
Joseph Jo <joseph.jo@coxinc.com>
2014-06-18 09:09:55 -0400

    Add scrabble_score function

---
+++ scrabble_score.py
@@ -0,0 +1,18 @@
+""" Print the scrabble score for a given word """
+def scrabble_score(word):
+    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+             "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+             "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+             "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+             "x": 8, "z": 10}
+    total = 0
+
+    for key in word:
+        key = key.lower()
+        total = total + score[key]
+
+    print "\"%s\" will earn you %s points in Scrabble." % (word, total)
+
+
+scrabble_score("Helix")
+scrabble_score("Qi")
