25ceb206d9710f6b2acf52dd8753c52e6c45042c
Joseph Jo <joseph.jo@coxinc.com>
2014-06-20 09:09:22 -0400

    Use double quotes for calc_raise function

--- calc_raise.py
+++ calc_raise.py
@@ -5,7 +5,7 @@ def calc_raise(current_salary, years, raise_percent):
 
     for x in xrange(1, years + 1):
         current_salary = current_salary * (raise_percent / 100 + 1)
-        current_salary_shortened = '%.2f' % current_salary
+        current_salary_shortened = "%.2f" % current_salary
 
         print "Year %s: $%s." % (x, current_salary_shortened)
 
