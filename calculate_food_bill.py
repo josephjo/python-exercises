f1de98c59220086b258adb6f2a9a003fb6a62e35
Joseph Jo <joseph.jo@coxinc.com>
2014-06-23 08:33:40 -0400

    Change function name back and add pretip to print for calculating food bill

--- calculate_food_bill.py
+++ calculate_food_bill.py
@@ -2,11 +2,11 @@
     Calculate tax and tip
     Tip is based on pre-tax amount.
 """
-def calc_tax(pretip, tax, tip_percent):
+def calc_food_bill(pretip, tax, tip_percent):
     total = (pretip * (1 + tip_percent / 100.0)) + (pretip * tax / 100.0)
     total = "%.2f" % total
 
-    print "The total for %s%% tip and %s%% tax is $%s." % (tip_percent, tax, total)
+    print "The total for $%s after %s%% tip and %s%% tax is $%s." % (pretip, tip_percent, tax, total)
 
 
-calc_tax(34.12, 6, 20)
+calc_food_bill(34.12, 6, 20)
