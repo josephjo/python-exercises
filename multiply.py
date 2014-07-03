62bdb48396574990185edd0b4f016df924662bf6
Joseph Jo <joseph.jo@coxinc.com>
2014-07-03 08:50:31 -0400

    Change multiply function to return rather than print

--- multiply.py
+++ multiply.py
@@ -1,10 +1,11 @@
-""" For a list of integers, print the product """
+""" For a list of integers, return the product """
 def multiply(numbers):
     total = 1
     for num in numbers:
-        total = total * num
+        total *= num
 
-    print "The total is %s." % total
+    return "The total is %s." % total
 
 
-multiply([10, 2, 1])
+print multiply([10, 2, 1])
+print multiply([91, 4, 23, 10, 0, 2, 1, 11])
