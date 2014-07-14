b541404c79058ac9eaa0fdc266a375028123561a
Joseph Jo <joseph.jo@coxinc.com>
2014-07-14 08:30:49 -0400

    Add a random character generator

---
+++ rand_gen.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+
+""" Python function to generate text via random chars and ints """
+def rand_gen(num):
+    rand = []
+
+    for i in range(num):
+        rand += random.choice(string.letters)
+
+    rand_string = "".join(rand)
+    return rand_string
+
+
+print rand_gen(4)
