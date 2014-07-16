4c1cb466b843e839b81931313a4b41d89df06016
Joseph Jo <joseph.jo@coxinc.com>
2014-07-16 08:52:52 -0400

    Modify calc_raise function to do a one time raise

--- calc_raise.py
+++ calc_raise.py
@@ -1,13 +1,25 @@
 """ Prints salary after x number of years for a certain percentage raise """
-def calc_raise(current_salary, years, raise_percent):
+def calc_raise_years(current_salary, years, raise_percent):
     print "Starting salary is $%s.\n" % current_salary
     print "Calculating salary for %s years with a %s%% raise:" % (years, raise_percent)
 
     for x in xrange(1, years + 1):
         current_salary = current_salary * (float(raise_percent) / 100 + 1)
-        current_salary_shortened = "%.2f" % current_salary
+        salary_formatted = "%.2f" % current_salary
+        print "Year %s: $%s." % (x, salary_formatted)
 
-        print "Year %s: $%s." % (x, current_salary_shortened)
+
+calc_raise_years(16640, 10, 3)
 
 
-calc_raise(16640, 10, 3)
+
+""" Prints salary after a percentage of raise """
+def calc_raise(current_salary, raise_percent):
+    new_salary = current_salary * (float(raise_percent) / 100 + 1)
+    new_salary_formatted = "%.2f" % new_salary
+
+    print "-------------------------------------\n\n "
+    return "Calculating salary $%s with a %s%% raise: $%s." % (current_salary, raise_percent, new_salary_formatted)
+
+
+print calc_raise(16640, 3)
