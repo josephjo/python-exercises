395e69852ad86d9252b1658a99954a89b73c2604
Joseph Jo <joseph.jo@coxinc.com>
2014-07-24 09:07:00 -0400

    Add a function to find the longest word in a list

---
+++ find_longest_word.py
@@ -0,0 +1,12 @@
+""" Find the longest word in a list of words. """
+def find_longest_word(list):
+    max_word = ""
+    max_length = 0
+
+    for word in list:
+        if len(word) > max_length:
+            max_word = word
+
+    print max_word, "is the longest word with %s letters." % len(word)
+
+find_longest_word(["cat", "dog", "giraffe"])
