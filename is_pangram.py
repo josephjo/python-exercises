d72eb20c47c81874adad0679a849925cae78db96
Joseph Jo <joseph.jo@coxinc.com>
2014-07-25 08:56:57 -0400

    Finish is_pangram and give several examples

--- is_pangram.py
+++ is_pangram.py
@@ -6,4 +6,20 @@ import string
     the alphabet at least once).
 """
 def is_pangram(s):
-    pass
+    alphabet = string.lowercase
+    s = s.lower()
+
+    for letter in s:
+        if letter in alphabet:
+            alphabet = alphabet.replace(letter, "")
+
+    if alphabet == "":
+        print "Yes, this is an pangram!"
+    else:
+        print "No, this is not an pangram. \"%s\" was not used." % alphabet
+
+
+is_pangram("Nymphs blitz quick vex dwarf jog") # True
+is_pangram("Cwm fjord veg balk nth pyx quiz") # False
+is_pangram("Dog") # False
+is_pangram("Grumpy wizards make toxic brew for the evil queen and jack") # True
