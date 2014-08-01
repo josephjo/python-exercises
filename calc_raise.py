ce22cf0b569cd1af30f2823a479389933f1d6088
Joseph Jo <joseph.jo@coxinc.com>
2014-08-01 10:06:51 -0400

    Limit columns to 80 chars

--- calc_raise.py
+++ calc_raise.py
@@ -1,7 +1,8 @@
 """ Prints salary after x number of years for a certain percentage raise """
 def calc_raise_years(current_salary, years, raise_percent):
     print "Starting salary is $%s.\n" % current_salary
-    print "Calculating salary for %s years with a %s%% raise:" % (years, raise_percent)
+    print "Calculating salary for %s years with a %s%% raise:" % (years, \
+        raise_percent)
 
     for x in xrange(1, years + 1):
         current_salary = current_salary * (float(raise_percent) / 100 + 1)
@@ -19,7 +20,8 @@ def calc_raise(current_salary, raise_percent):
     new_salary_formatted = "%.2f" % new_salary
 
     print "-------------------------------------\n\n "
-    return "Calculating salary $%s with a %s%% raise: $%s." % (current_salary, raise_percent, new_salary_formatted)
+    return "Calculating salary $%s with a %s%% raise: $%s." % (current_salary, \
+        raise_percent, new_salary_formatted)
 
 
 print calc_raise(16640, 3)
