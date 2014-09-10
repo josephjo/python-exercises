7936420bb237dc1db0b8a928942e2213099d50fd
Joseph Jo <joseph.jo@coxinc.com>
2014-09-10 13:09:57 -0400

    Get user input for cost, miles, and fees

--- calc_air_miles.py
+++ calc_air_miles.py
@@ -1,10 +1,13 @@
 """ Calculate cost per mile for miles needed for a free flight minus any fees """
-def calc_air_miles(cost, miles, fees):
+def calc_air_miles():
+    cost = input("Amount you would pay for the flight: $");
+    miles = input("Amount of miles it costs: ")
+    fees = input("Amount of fees associated with redeeming with miles: $")
+
     total = (float(cost - fees) / miles) * 100
     total_formatted = "%.2f" % total
 
     return "You're getting %s cents per mile for a $%s ticket." % (total_formatted, cost)
 
 
-print calc_air_miles(300, 25000, 20) # 1.12 cpm
-print calc_air_miles(16200, 200000, 300) # 7.95 cpm
+print calc_air_miles()
